90055265944cefff39b3cfbfd1c9fbeb42910423
Matthew <idiyampro@gmail.com>
2020-04-23 01:03:30 +0300

    Fix test directory in unit-tests.yml

--- test/util.py
+++ test/util.py
@@ -1,11 +1,13 @@
 import os
 import subprocess
+import sys
 
-
-PATH = os.path.join(os.getcwd(), 'slrpn.py')
+PYTHON_INTERPRETER_PATH = sys.executable
+SCRIPT_PATH = os.path.join(os.getcwd(), 'slrpn.py')
 
 
 def call(expression: str) -> float:
     args = expression.split()
-    raw_out = subprocess.check_output(('python', PATH, *args))
+    raw_out = subprocess.check_output((PYTHON_INTERPRETER_PATH, SCRIPT_PATH,
+                                       *args))
     return float(raw_out.decode('utf8'))
